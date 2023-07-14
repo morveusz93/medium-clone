2353f4639a7218e02281143ef4cb0b7730b90d5d
morveusz93 <morveusz93@gmail.com>
2023-07-14 20:57:06 +0200

    przeniesienie modelu author do artykułu

--- articles/models.py
+++ articles/models.py
@@ -1,15 +1,26 @@
 from django.db import models
-from authors.models import Author
 
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateField(auto_now_add=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} - by {self.author}"
 
     class Meta:
         ordering = ['-pub_date']
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name
--- medium/settings.py
+++ medium/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "articles.apps.ArticlesConfig",
-    "authors.apps.AuthorsConfig",
 ]
 
 MIDDLEWARE = [
