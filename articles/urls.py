ee28d1a619bc5d4020e4de3b468f18af7401f27e
morveusz93 <morveusz93@gmail.com>
2023-07-15 07:24:37 +0200

    widok tagu

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from articles.views import ArticleDetailView, ArticlesListView
+from articles.views import ArticleDetailView, ArticleListView, TagListView, TagDetailView
 
 urlpatterns = [
     # path('', HomePageView.as_view(), name='home'),
-    path('articles/<str:pk>/', ArticleDetailView.as_view(), name='article-details'),
-    path('articles/', ArticlesListView.as_view(), name='home'),
+    path('articles/<str:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('articles/', ArticleListView.as_view(), name='home'),
+    path('tags/', TagListView.as_view(), name='tag-list'),
+    path('tags/<str:pk>/', TagDetailView.as_view(), name='tag-detail'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,32 @@
 from django.views.generic import DetailView, ListView
-from articles.models import Article
+from django.views.generic.list import MultipleObjectMixin
+
+from articles.models import Article, Tag
 
 
-class ArticlesListView(ListView):
+class ArticleListView(ListView):
     model = Article
-    context_object_name = 'articles_list'
+    context_object_name = 'article_list'
     paginate_by = 5
 
 
-class ArticleDetailView( DetailView):
+class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'article'
+
+
+class TagListView(ListView):
+    model = Tag
+    context_object_name = 'tag_list'
+    paginate_by = 5
+
+
+class TagDetailView(DetailView, MultipleObjectMixin):
+    model = Tag
+    context_object_name = 'tag'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        object_list = Article.objects.filter(tags=self.object).all()
+        context = super(TagDetailView, self).get_context_data(object_list=object_list, **kwargs)
+        return context
